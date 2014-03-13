713808a4e4b89cacb5f0c6f880849b9cd080a375
BethRandall <brandall@sas.upenn.edu>
2014-03-12 22:13:03 -0400

    new version number

--- GrammerTrainer/ViewController.m
+++ GrammerTrainer/ViewController.m
@@ -88,7 +88,7 @@
 @synthesize loginInfo = loginInfo_;
 @synthesize versionLabel = versionLabel_;
 @synthesize resultsDict = resultsDict_;
-static NSString *versionNumber = @"1.10";
+static NSString *versionNumber = @"1.11";
 
 - (void)didReceiveMemoryWarning
 {
