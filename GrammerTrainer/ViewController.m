b93d495ba3cc295b38b766a120acec83b25561e4
BethRandall <brandall@sas.upenn.edu>
2014-10-30 13:39:14 -0400

    SentenceWeaver 1.20, working provisioning profile

--- GrammerTrainer/ViewController.m
+++ GrammerTrainer/ViewController.m
@@ -95,7 +95,7 @@
 @synthesize slt;
 
 
-static NSString *versionNumber = @"1.18";
+static NSString *versionNumber = @"1.20";
 
 - (FliteController *)fliteController { if (fliteController == nil) {
     fliteController = [[FliteController alloc] init]; }
