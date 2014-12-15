3be1dd4c40f0dcffadd49007a4ce8b511642db7a
BethRandall <brandall@sas.upenn.edu>
2014-12-15 10:20:19 -0500

    version 1.29

--- GrammerTrainer/ViewController.m
+++ GrammerTrainer/ViewController.m
@@ -95,7 +95,7 @@
 @synthesize slt;
 
 
-static NSString *versionNumber = @"1.28";
+static NSString *versionNumber = @"1.29";
 
 - (FliteController *)fliteController { if (fliteController == nil) {
     fliteController = [[FliteController alloc] init]; }
