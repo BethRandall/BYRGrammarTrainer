05db97181b9a62d061cef6a7efe299e316f394c1
BethRandall <brandall@sas.upenn.edu>
2015-01-18 17:26:29 -0500

    one more "boys says"

--- GrammerTrainer/ViewController.m
+++ GrammerTrainer/ViewController.m
@@ -95,7 +95,7 @@
 @synthesize slt;
 
 
-static NSString *versionNumber = @"1.33";
+static NSString *versionNumber = @"1.34";
 
 - (FliteController *)fliteController { if (fliteController == nil) {
     fliteController = [[FliteController alloc] init]; }
